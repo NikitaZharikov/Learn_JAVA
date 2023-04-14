0e2021ff6aacc4ea50f90c0deb749970722e9838
NikitaZharikov <nikitkazharikov@gmail.com>
2023-04-14 12:00:58 +0300

    first program on java

---
+++ test/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedite chislo");
+        int a = scanner.nextInt();
+        System.out.println(subtractProductAndSum(a));
+        scanner.close();
+    }
+
+    /**
+     * @param a input int
+     * @return out sum
+     */
+    public static int subtractProductAndSum(int a){
+        int product = 1;
+        int sum = 0;
+        while (a != 0)
+        {
+            product *= a % 10;
+            sum += a % 10;
+            a /= 10;
+        }
+        return product - sum;
+    }
+}
