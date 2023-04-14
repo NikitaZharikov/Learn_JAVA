6eb57d937d36e2a5df1c2bdbe6fb0a7d5fe32ca4
NikitaZharikov <nikitkazharikov@gmail.com>
2023-04-14 12:06:13 +0300

    test commit

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedite chislo");
+        System.out.println("Vvedite chislo gg");
         int a = scanner.nextInt();
         System.out.println(subtractProductAndSum(a));
         scanner.close();
